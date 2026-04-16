3e327ea0e341f3e0d5ed5c3b60d792511a76182e
Gabriel Silva <gabrielmoreira9699@gmail.com>
2026-04-16 15:47:32 -0300

    Adicionar arquivos de projeto.

---
+++ Biblioteca de Jogos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Biblioteca_de_Jogos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
